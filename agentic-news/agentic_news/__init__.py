986d5253ebc1a66b39d81a74e1f61a07811a718b
rajkstats <raj.k.stats@gmail.com>
2025-02-23 04:58:36 +0530

    Remove duplicate agentic-news directory

--- agentic-news/agentic_news/__init__.py
+++
@@ -1,5 +0,0 @@
-from .agent import NewsAgent
-from .providers import LiteLLMProvider
-from .utils.logger import Logger
-
-__all__ = ['NewsAgent', 'LiteLLMProvider', 'Logger'] 
--- agentic-news/agentic_news/utils/logger.py
+++
@@ -1,7 +0,0 @@
-class Logger:
-    def __init__(self):
-        self.log_file = None
-
-    def log(self, message, color=None):
-        print(message)
-        return message 
